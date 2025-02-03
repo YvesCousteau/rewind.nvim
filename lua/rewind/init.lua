5293d362e461abead49edafe2dd3e6a69a79adcb
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-02-03 10:49:06 +0100

    WIP

--- lua/rewind/init.lua
+++ lua/rewind/init.lua
@@ -1,1 +1,16 @@
-print("hello from our plugin")
+local augroup = vim.api.nvim_create_augroup("Rewind", {
+	clear = true, -- clears the previous autocommands instead of creating multiple autocommands.
+})
+
+local function main()
+	print("Hello from our plugin")
+end
+
+local function setup()
+	vim.api.nvim_create_autocmd(
+		"VimEnter",
+		{ group = augroup, desc = "Set a fennel scratch buffer on load", once = true, callback = main }
+	)
+end
+
+return { setup = setup }
