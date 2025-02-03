36adc84d169b6d9a9586a1eb2ae1a16b152be533
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-02-03 11:25:09 +0100

    wip

--- lua/rewind/init.lua
+++ lua/rewind/init.lua
@@ -1,13 +1,20 @@
 local M = {}
 
+function M.say_hello()
+	print("Hello from Neovim!")
+end
+
 function M.setup(opts)
-	vim.api.nvim_create_user_command("Rewind", function(input)
-		print("Something should happen here...")
-	end, {
-		{
-			bang = true, -- "!" after the cmd is allowed.
-			desc = "a new command to do the thing",
-		},
+	-- Merge user options with defaults
+	opts = opts or {}
+
+	vim.api.nvim_create_user_command("Rewind", M.say_hello, {})
+
+	local keymap = opts.keymap or "<leader>hw"
+
+	vim.keymap.set("n", keymap, M.say_hello, {
+		desc = "Say hello from our plugin",
+		silent = true, -- Prevents the command from being echoed in the command line
 	})
 end
 
