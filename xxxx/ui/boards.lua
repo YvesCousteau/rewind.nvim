b906bc077ad1cfa16226640afaf57b60aa72dce9
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-02-06 14:00:53 +0100

    D

--- xxxx/ui/boards.lua
+++ xxxx/ui/boards.lua
@@ -18,6 +18,17 @@ function M.create_window(buf, win, config)
 
 	api.nvim_buf_set_lines(buf.boards, 0, -1, false, rewind.util.get_boards())
 	api.nvim_buf_set_option(buf.boards, "modifiable", false)
+
+	rewind.autocmd.cursor_move(buf.boards, function()
+		print("ldsjf")
+		rewind.util.update_highlight(buf.boards, rewind.state.highlight_namespace)
+		local current_board = api.nvim_get_current_line()
+		rewind.util.update_contents(buf.lists, rewind.controller.lists.get(current_board))
+		rewind.util.update_contents(
+			buf.tasks,
+			rewind.controller.lists.get(current_board, rewind.controller.lists.get_first(current_board))
+		)
+	end)
 end
 
 return M
