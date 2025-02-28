9c5f039ae1d8d1c2f507d941e8b141168970094a
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-01 00:08:17 +0100

    ADD instruction

--- lua/rewind/state.lua
+++ lua/rewind/state.lua
@@ -10,6 +10,7 @@ local M = {
 		"date_picker",
 		"status",
 		"tags",
+		-- les tags sont liee au boards ou a la list et les tasks contiennet les tags si tags supp de la list alors tej de chaque task
 	},
 }
 
