e34f0afd184e69116ffe5849a2432f54db547055
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-01 15:40:14 +0100

    WIP

--- lua/rewind/ui/tags.lua
+++ lua/rewind/ui/tags.lua
@@ -9,15 +9,20 @@ function M.open_window(key)
 
 	local _, board = util.get_cursor_content("boards")
 	local buf = util.buf.get(key)
+
 	if buf and board and board.tags and #board.tags > 0 then
 		for id, tag in pairs(board.tags) do
 			if tag.title then
-				vim.api.nvim_set_hl(2, key, { fg = "#FF0000" })
+				-- local ns = util.get_var(board.title .. tag.title .. "ns")
+				-- util.set_var(key .. "ns", ns)
+				local ns = vim.api.nvim_create_namespace(board.title .. tag.title)
+
+				vim.api.nvim_set_hl(ns, key, { fg = "#FF0000" })
 				-- local line = id - 1
 				-- local col_start = 0
 				-- local col_end = #tag.title
 				-- vim.api.nvim_buf_add_highlight(buf, 0, tag.title, line, col_start, col_end)
-				vim.api.nvim_buf_add_highlight(buf, 2, key, id - 1, 0, -1)
+				vim.api.nvim_buf_add_highlight(buf, ns, key, id - 1, 0, -1)
 			end
 		end
 	end
